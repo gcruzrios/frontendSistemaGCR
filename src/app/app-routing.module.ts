b6ce91c10c825019f84115f83db6752809199848
Greivin Cruz <gcruzr@gmail.com>
2020-05-19 11:17:04 -0600

    Version actualizada del Front End

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: PagesComponent,
+    children :[
+        {path: 'dashboard', component: DashboardComponent},
+        {path: '', redirectTo:'/dashboard', pathMatch: 'full'},
+    ]
+  },
+  
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  
+  {path: '**',  component: NopagefoundComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true})],
   exports: [RouterModule]
 })
+
+//export const APP_ROUTES = RouterModule.forRoot(routes, { useHash: true});
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//APP_ROUTES
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    RegisterComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
